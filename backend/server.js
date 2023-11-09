923575b3b994c2214ed7f6589441d9c2aba32c30
heli2309 <123007540+heli2309@users.noreply.github.com>
2023-11-10 00:11:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,11 @@ mongoose.connect('mongodb://127.0.0.1:27017/cricket', {
   useUnifiedTopology: true,
 });
 
-app.use(cors());
+app.use(cors(
+  origin:["https://deploy-mern.vercel.app"],
+  methods:['POST','GET'],
+  credentials:true
+));
 app.use(bodyParser.json());
 
 const matchSchema = new mongoose.Schema({
